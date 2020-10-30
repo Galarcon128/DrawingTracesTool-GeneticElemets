139b3ff4dcafbfbb9f3345f7b5466e59163496c5
Lizeth Arizmendi <69496233+Lizeth-Ariz@users.noreply.github.com>
2020-10-29 21:01:21 -0600

    RNA/TF_binding RNA finalizado

--- src/GeneticElements/TF_binding_site.js
+++ src/GeneticElements/TF_binding_site.js
@@ -9,41 +9,50 @@ let adnX = 0;
 let adnY = 100;
 let adnSize = canvaW;
 let adnScalar = 1000; //bp on adn track
-let separation = 10;
+let separation = 30;
 let x = 0; //leftPosition
 // gene data
-let name = "rna";
+let name = "Tf binding";
 let size = 10;
-let strand = "forwar"; // default forward
+let strand = "revers"; // default forward
 
 // draw data
-let color = "red";
+let color = "#000";
 let stroke = { color: "#000", width: 3, linecap: "round", linejoin: "round" };
 let sizeP = (size * adnSize) / adnScalar;
 if (sizeP <= 20) {
   sizeP = 20;
 }
 //Canvas Create
-const draw = SVG().addTo("#rna").size(adnSize, canvaH);
+const draw = SVG().addTo("#TFbindingsite").size(adnSize, canvaH);
 // DNA Create
 const adn = draw
   .line(adnX, adnY, adnSize, adnY)
   .stroke({ color: "#f06", width: 2, linecap: "round" });
 
-const rnaH = 60;
+//draw RNA
+const tf_binding = 60;
 
-var rna = draw.rect(rnaH, rnaH / 3);
+var rna = draw.rect(tf_binding, tf_binding / 3);
 
 //position & stroke
-let y = adnY - separation;
+let y = adnY - separation - 20;
 let xi = x + adnX;
-rna.fill(color);
+rna.fill("none");
 rna.stroke(stroke).move(xi, y);
 
-//draw lines
-const lx = x + adnX;
-const ly = adnY - separation - rnaH / 3 - 20;
-var linea1 = draw
-  .line(10, 0, 10, rnaH / 3)
-  .stroke(stroke)
-  .move(lx + 60, ly);
+// name draw
+const text = draw.text(name);
+text.font({
+  family: "Arial",
+  size: 18,
+  separation: "middle"
+});
+
+//strand effect
+if (strand === "reverse") {
+  rna.rotate(180).move(xi, adnY - separation - tf_binding - 20);
+  text.move(xi + tf_binding / 4, adnY + tf_binding - 10);
+} else {
+  text.move(xi + tf_binding / 4, adnY - tf_binding - 10);
+}
