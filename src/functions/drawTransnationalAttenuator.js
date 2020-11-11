5f724c5916e82440601db7db57c050bd4a8be99c
Lizeth Arizmendi <69496233+Lizeth-Ariz@users.noreply.github.com>
2020-11-11 14:52:22 -0600

    TransnationalAttenuator changes by Gabriel

--- src/functions/drawTransnationalAttenuator.js
+++ src/functions/drawTransnationalAttenuator.js
@@ -1,79 +1,105 @@
+// TransAtt 0.5.0
+/**
+ * Falta el Return, y utilizar anchor
+ *
+ */
 export default function DrawTransnationalAttenuator({
+  id,
   canva,
-  adnX = 0,
-  adnY = 100,
-  adnSize = 200,
-  adnScalar = 1000,
-  separation = 0,
-  x = 0,
-  name = "Name",
-  size = 100,
+  anchor,
+  dna,
+  separation = 20,
+  posLeft = 10,
+  posRigth = 50,
+  name = "geneName",
   strand = "forward",
-  color = "none",
+  color = "aqua",
   opacity = 1,
   stroke = { color: "#000", width: 1, linecap: "round" }
 }) {
-  if (!canva) {
+  if (!canva || !dna || !id || posLeft > posRigth) {
     return null;
   }
-  let sizeP = (size * adnSize) / adnScalar;
 
-  var altura = 60 + separation;
-  let hline = 1;
-  if (sizeP >= 20) {
-    hline = sizeP / 2 - 9;
+  // atributos
+  const dnaX = dna.x,
+    dnaY = dna.y,
+    size = posRigth - posLeft,
+    widthActive = dna.widthActive,
+    dnaSize = dna.Size,
+    x = ((posLeft - dna.posLeft) * widthActive) / dnaSize;
+  let sizeP = (size * widthActive) / dnaSize;
 
-    var body = canva.path(
-      "M 0,0 v " +
-        altura +
-        " h -" +
-        hline +
-        " v 5 h " +
-        sizeP +
-        " v -5 h -" +
-        hline +
-        " v " +
-        -altura
-    );
-    let bodyX = x + adnX;
-    let bodyY = adnY - altura - 5;
-    body.fill(color).move(bodyX, bodyY);
-    body.stroke(stroke);
-    body.opacity(opacity);
-
-    let headH = 30;
-    var head = canva.circle(headH);
-    let headX = adnX + x + (hline - 6);
-    let headY = adnY - altura - 30;
-    head.fill(color).move(headX, headY);
-    head.stroke(stroke);
-    head.opacity(opacity);
-
-    var lx = sizeP / 4;
-    if (lx < 20) {
-      lx = 15;
-    } else if (lx >= 25) {
-      lx = 20;
-    }
-    var ly = sizeP / 2;
-    if (ly < 20) {
-      ly = 30;
-    } else if (ly >= 50) {
-      ly = 30;
-    }
-    var rect = canva.rect(lx, ly);
-
-    let rectX = adnX + x + (hline + lx - 5);
-    let rectY = adnY - altura + 10;
-    rect.fill(color).move(rectX, rectY);
-    rect.stroke(stroke);
-
-    if (strand === "reverse") {
-      var group = canva.group();
-      group.add(head);
-      group.add(body);
-      group.add(rect);
-      group.rotate(180).move(x + adnX, headY - altura - 30);
-    }
+  //escala
+  let heigthActive = dna.forwardActive;
+  if (strand === "reverse") {
+    heigthActive = dna.reverseActive;
   }
+  const proportion = heigthActive * 0.1;
+  //atributos de Cuerpo
+  let bodyHeigth = proportion * 3 + separation;
+  let bodyFootH = proportion / 4;
+  let bodyFootW = 0;
+  if (sizeP >= proportion) {
+    bodyFootW = sizeP / 2 - proportion / 3;
+  }
+  let bodyX = x + dnaX;
+  let bodyY = dnaY - bodyHeigth - bodyFootH;
+  //atributos de Cabeza
+  let headH = proportion;
+  let headX = dnaX + x + sizeP / 2 - headH / 2;
+  let headY = dnaY - bodyHeigth - headH;
+  // atributos del rectangulo
+  let rectWidth = proportion / 2;
+  let rectHeigth = proportion * 2;
+  let rectX = dnaX + x + sizeP - bodyFootW;
+  let rectY = dnaY - bodyHeigth;
+
+  // dibujo de  BODY
+  const body = canva.path(
+    "M 0,0 v " +
+      bodyHeigth +
+      " h -" +
+      bodyFootW +
+      " v " +
+      bodyFootH +
+      " h " +
+      sizeP +
+      " v -" +
+      bodyFootH +
+      " h -" +
+      bodyFootW +
+      " v " +
+      -bodyHeigth
+  );
+  body.fill(color).move(bodyX, bodyY);
+  body.stroke(stroke);
+  body.opacity(opacity);
+  // dibujo de HEAD
+  const head = canva.circle(headH);
+  head.fill(color).move(headX, headY);
+  head.stroke(stroke);
+  head.opacity(opacity);
+  // dibujo de RECT
+  const rect = canva.rect(rectWidth, rectHeigth);
+  rect.fill(color).move(rectX, rectY);
+  rect.stroke(stroke);
+  // reverse effect
+  if (strand === "reverse") {
+    var group = canva.group();
+    group.add(body);
+    //group.add(rect);
+    group.add(head);
+    group.transform({
+      rotate: 180,
+      translateY: bodyHeigth + headH
+    });
+    rect.transform({
+      rotate: 180,
+      translateX: -proportion - rectWidth / 3,
+      translateY: bodyHeigth + headH + separation
+    });
+  }
+  //anchor effect
+  //returns :C
 }
